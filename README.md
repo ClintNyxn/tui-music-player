559a106284c6e7aeb97a261ca9766337f724e7e9
ClintNyxn <nyxnclint@gmail.com>
2026-09-11 21:59:05 +0530

    added pause/play and next/prev and quit and improved readme.md

--- README.md
+++ README.md
@@ -8,8 +8,32 @@ miro - overview flow diagram & wireframe
 - made os-based playing system
 
 11 sept friday
+- [x] only one song plays at a time
+- [x] formatted 
+- [] pausing / playing
+- [] prev / next
+- [] added ui 
+
+12 sept saturday
+
+
+13 sept sunday
+
+
+## future improvement options
+1. pause and play for linux since it's using mpv
+a tui for listening to music
+
+miro - overview flow diagram & wireframe 
+
+10 sept thursday
+- added vim bindings to navigate bw songs
+- find songs and put into useful forms
+- made os-based playing system
+
+11 sept friday
 [x] only one song plays at a time
-[] formatted 
+[x] formatted 
 [] pausing / playing
 [] prev / next
 [] added ui 
@@ -18,3 +42,7 @@ miro - overview flow diagram & wireframe
 
 
 13 sept sunday
+
+
+## future improvement options
+1. pause and play for linux since it's using mpv
--- index.js
+++ index.js
@@ -17,7 +17,32 @@ function main() {
   console.log(song_names);
 
   let i = 0;
+  let paused = false 
   let curr_play = null;
+
+  function play_this(i){
+    console.log(`playing ${song_names[i]}`);
+    const song_path = path.join(path_to_music, music_files[i]);
+    let program = os.platform() == "darwin" ? "afplay" : "mpv";
+
+    if (curr_play) { curr_play.kill()}
+
+    curr_play = spawn(program, [song_path]);
+    curr_play.stdout.on("data", (i) => {
+      console.log(i.toString())
+    })
+  }
+
+  function kill_this(curr_play){
+    if (paused){
+      curr_play.kill("SIGCONT")
+      paused = false
+    }else{
+      curr_play.kill("SIGSTOP") 
+      paused = true
+    }
+  }
+
 
   process.stdin.on("data", (chunk) => {
     let user_input = chunk.toString().trim();
@@ -27,17 +52,34 @@ function main() {
     } else if (user_input === "k" && i > 0) {
       i -= 1;
     } else if (user_input == "p") {
-      console.log(`playing ${song_names[i]}`);
-      const song_path = path.join(path_to_music, music_files[i]);
-      let program = os.platform() == "darwin" ? "afplay" : "mpv";
+      play_this(i)
+    }
 
+    else if (user_input == "P"){ // no functionality for linux yet
+      kill_this(curr_play)
+    }
+
+    else if (user_input == "L"){
       if (curr_play) {
         curr_play.kill();
+        i += 1;
+        play_this(i)
       }
-      curr_play = spawn(program, [song_path]);
-      curr_play.stdout.on("data", (i) => {
-        console.log(i.toString());
-      });
+    }
+    else if (user_input == "H"){
+      if (curr_play) {
+        curr_play.kill();
+        i -= 1;
+        play_this(i)
+      }
+    }
+    else if (user_input == "q"){
+      console.log("Exited Player")
+      curr_play.kill();
+      process.exit(0);
+    }
+    else{
+      console.log("That isn't a command")
     }
   });
 }
